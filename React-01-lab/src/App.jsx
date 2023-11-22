cf26648ce10e117548fe489e6a98e875939204f9
MaanRaddadi <maan.raddadi@gmail.com>
2023-11-22 15:51:19 +0300

    ,

--- React-01-lab/src/App.jsx
+++ React-01-lab/src/App.jsx
@@ -14,7 +14,7 @@ function App(props) {
  </Companies>
  <Companies compName="Tuwaiq" emp="30" year="2020" name="Fahad" name2="Omar" name3="Osama" lang="C++, Java, C#" lang2="C, C++" lang3="Python" exp="15 Years" exp2="10 Years" exp3="5 Years" service="Education" price="Free" comp="Tuwaiq">
  </Companies>
- <Companies compName="Aramco" emp="400" year="1970" name="Nora" name2="Zaid" name3="Lujain" lang="C++, Java, C#" lang2="C, Python" lang3="Python" exp="15 Years" exp2="10 Years" exp3="5 Years" service="Internet" price="from 1500SAR Up to 55000SAR" comp="Aramco">
+ <Companies compName="Aramco" emp="400" year="1970" name="Nora" name2="Zaid" name3="Lujain" lang="C++, Java, C#" lang2="C, Python" lang3="Python" exp="15 Years" exp2="10 Years" exp3="5 Years" service="Oil and Enrgy" price="from 1500SAR Up to 55000SAR" comp="Aramco">
  </Companies>
    </div>
   
